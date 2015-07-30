54ca8e8aa41fac772f4da2ce10a1557ebff59981
Matt Zumwalt <matt@curationexperts.com>
2015-07-30 14:14:23 -0700

    pin elasticsearch package version

--- databindery/recipes/elastic_setup.rb
+++ databindery/recipes/elastic_setup.rb
@@ -1,8 +1,13 @@
 include_recipe 'databindery::apt'
 
 packages = %w(
-  elasticsearch openjdk-7-jdk
+  openjdk-7-jdk
 )
+
+package 'elasticsearch' do
+  version '1.6.2' # to avoid bug in 1.6.0 https://github.com/elastic/elasticsearch/issues/11594
+  action :install
+end
 
 packages.each { |name| package name }
 
